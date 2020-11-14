921376675b85b4ded38ba122e911d58aeff30a28
Paweł Kowalik <01112962@pw.edu.pl>
2020-11-14 12:35:34 +0100

    algorith corrected, comments added

--- lambertian.py
+++ lambertian.py
@@ -3,36 +3,52 @@ import numpy as np
 from scipy import interpolate
 import random
 
+"""
+calculating generalized Lambertian radiant intensity model used to approximate the radiation pattern of light emitter
+:param N: number of reflectances
+:param m: mode number of the radiation lobe which specifies the directionality of the emitter == 1
+"""
 def calculate(N,m):
 
     AS1 = []
-    Rs = []
+    Re = []
     p = []
     indices = []
     AS1prim = []
     AS = []
     AS2 = []
+
     for x in range(-90,91):
         AS1.append(x)
 
     for as1 in AS1:
-        Rs.append(math.pow(((m+1)/(2*math.pi)*math.cos(math.radians(as1))),m))
-       
-    for rs in Rs:
-        p.append(rs/sum(Rs))
+        # generalized Lambertian radiation pattern
+        Re.append(((m+1)/(2*math.pi))*math.pow(math.cos(math.radians(as1)),m))
+  
+    
+    # array with fractions
+    for re in Re:
+        p.append(re/sum(Re))
  
-    rdf = np.cumsum(p) # sum of all dimensions
-    rdf, indices = np.unique(rdf, return_index=True) # unique values from an array
+    # reflectance distribution function, sum of all dimensions
+    rdf = np.cumsum(p) 
 
+    # unique values from an array with their indices
+    rdf, indices = np.unique(rdf, return_index=True) 
+
+    #onlu unique values
     for indice in indices:
         AS1prim.append(AS1[indice])
 
+    #random value between 1 and N
     AS2 = np.random.uniform(0, 1, int(N))
 
+    #interpolation function
     f = interpolate.interp1d(rdf, AS1prim)
 
-    for points in AS2:
-        AS.append(f(points))
+    # using interpolation function on query points
+    for query_points in AS2:
+        AS.append(f(query_points))
 
     return AS
 
--- runner.py
+++ runner.py
@@ -1,4 +1,4 @@
-import mmc_algorithm
+import modified_monte_carlo_algorithm
 import random_generator
 import results_saver
 import random
@@ -7,11 +7,11 @@ import json
 
 max_room_size = 4
 parameters = []
-
-number_of_input_values = 200
+draw_chart = True
+number_of_input_values = 1
 for i in range(number_of_input_values):
     single_simulation_parameters = random_generator.generate_random_input_values(max_room_size)
-    single_channel_impulse_response = mmc_algorithm.solve(single_simulation_parameters)
+    single_channel_impulse_response = modified_monte_carlo_algorithm.compute(single_simulation_parameters, draw_chart)
     parameters.append(single_simulation_parameters + single_channel_impulse_response)
     print(i) 
 
